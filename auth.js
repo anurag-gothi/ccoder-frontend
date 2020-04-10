ac694063b9b08a835c13891de384e5e73cc08a32
Tejas <tejaspvg@gmail.com>
2020-04-10 19:18:55 +0530

    Small Updates

--- auth.js
+++ auth.js
@@ -97,7 +97,18 @@ const markups = {
 			</div>
 		</div>
 	</div>
-`
+`,
+ dashboard: `
+ <div class="dashboard">
+<div class="card w-50">
+<div class="card-body">
+<h5 class="card-title">Card title</h5>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<a href="#" class="btn btn-primary">Button</a>
+</div>
+</div>
+</div> 
+ `
 };
 
 const app = document.querySelector("#app");
